792187ff7015b929324a5825c6bba30972a1e12a
cimpapps <catalincimpoeru24@gmail.com>
2019-06-07 23:37:54 +0300

    add target

--- src/main/java/com/example/hellocatalin/HelloCatalinApplication.java
+++ src/main/java/com/example/hellocatalin/HelloCatalinApplication.java
@@ -1,13 +1,19 @@
 package com.example.hellocatalin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class HelloCatalinApplication {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(HelloCatalinApplication.class, args);
@@ -19,4 +25,13 @@ public class HelloCatalinApplication {
         return "hello";
     }
 
+    @GetMapping("/back")
+    public String back() {
+       return restTemplate.getForEntity("http://localhost:8282", String.class).getBody();
+    }
+
+    @Bean
+    public  RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
